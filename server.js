a0c14070ca5c757451ede4a80199c157bd5fa612
Sarobidy <ratsaraefadahysarobidy@gmail.com>
2024-07-04 07:01:57 +0300

    port

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(prefix + '/Exercices', exercices);
 app.use(prefix + '/Planification', planification);
 
 // Obligatoire si déploiement dans le cloud !
-let port = process.env.PORT || 8010;
+let port = process.env.PORT || 10001;
 app.listen(port, "0.0.0.0");
 console.log('Serveur démarré sur http://localhost:' + port);
 
