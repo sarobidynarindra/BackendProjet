c0fe1022d2e8cf744b4bb853ec5ddcda714983e9
Sarobidy <ratsaraefadahysarobidy@gmail.com>
2024-07-04 07:12:25 +0300

    server

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-let express = require('express');
+const express = require('express');
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-let app = express();
-
+const app = express();
 
 const authRoutes = require('./routes/authRoutes');
 const formationClientRoutes = require('./routes/formationClientRoutes');
 const exercices = require('./routes/ExercicesRoutes');
 const planification = require('./routes/PlanificationRoutes');
-let bodyParser = require('body-parser');
-
+const bodyParser = require('body-parser');
 const multer = require('multer');
 
 const options = {
@@ -25,7 +23,29 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   logging: false, // Désactiver le logging de Sequelize (optionnel)
 });
 
-// Synchroniser les modèles avec la base de données
+// Middleware pour body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Middleware pour CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+// Définir les routes
+const prefix = '/api';
+app.use(prefix + '/auth', authRoutes);
+app.use(prefix + '/getAllFormation', formationClientRoutes);
+app.use(prefix + '/Exercices', exercices);
+app.use(prefix + '/Planification', planification);
+
+// Obligatoire si déploiement dans le cloud !
+const port = process.env.PORT || 10001;
+
+// Synchroniser les modèles avec la base de données et démarrer le serveur
 (async () => {
   try {
     await sequelize.authenticate();
@@ -34,30 +54,12 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     await sequelize.sync(); // Synchroniser les modèles avec la base de données
 
     // Démarrer le serveur une fois que tout est configuré
-    app.listen(port, () => {
+    app.listen(port, '0.0.0.0', () => {
       console.log(`Serveur en écoute sur le port ${port}`);
     });
   } catch (error) {
     console.error('Erreur de connexion à la base de données:', error);
   }
 })();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// les routes
-const prefix = '/api';
-// Ajouter les routes
-app.use(prefix + '/auth', authRoutes);
-app.use(prefix + '/getAllFormation', formationClientRoutes);
-app.use(prefix + '/Exercices', exercices);
-app.use(prefix + '/Planification', planification);
-
-// Obligatoire si déploiement dans le cloud !
-let port = process.env.PORT || 10001;
-app.listen(port, "0.0.0.0");
-console.log('Serveur démarré sur http://localhost:' + port);
-
 
 module.exports = app;
-
-
